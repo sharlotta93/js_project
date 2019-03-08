0dd0fbb15536476fe0eb40b468e633417b82ebb4
Abigaila Ekierte <arekiert@gmail.com>
2019-03-08 14:36:14 +0000

    setting up server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,26 @@
+const express = require("express");
+const app = express();
+const MongoClient = require("mongodb").MongoClient;
+const path = require("path");
+const parser = require("body-parser");
+const createRouter = require("./helpers/create_router.js");
 
+const publicPath = path.join(__dirname, "../client/public");
+app.use(express.static(publicPath));
+app.use(parser.json());
+
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then( ( client ) => {
+    const db = client.db('game');
+    const bucketlistCollection = db.collection('question');
+    const bucketlistRouter = createRouter(bucketlistCollection);
+    app.use('/api/game', bucketlistRouter);
+  })
+  .catch(console.error);
+
+
+
+app.listen(3000, function () {
+  console.log(`listening on port ${this.address().port}`);
+});
