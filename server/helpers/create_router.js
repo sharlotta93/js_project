1c7f2891205e93be2c4d7cac1fc23446209a7410
Abigaila Ekierte <arekiert@gmail.com>
2019-03-08 14:39:07 +0000

    create delete router

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -58,11 +58,18 @@ const createRouter = function ( collection ) {
         });
     });
 
-
-
-
-
-
+    router.delete('/:id', (req, res) => {
+      const id = req.params.id;
+      collection
+        .deleteOne({_id: ObjectID(id)})
+        .then( ()=> collection.find().toArray())
+        .then( (docs) => res.json(docs) )
+        .catch((err) => {
+          console.error(err);
+          res.status(500);
+          res.json({status: 500, error: err});
+        });
+    })
 
   return router;
 };
