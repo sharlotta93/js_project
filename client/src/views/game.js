abc4fe65273e12b991fcb809fd5739b404a2a02d
Abigaila Ekierte <arekiert@gmail.com>
2019-03-09 11:33:49 +0000

    adding game view

--- client/src/views/game.js
+++ client/src/views/game.js
@@ -1,1 +1,21 @@
+const PubSub = require("../helpers/pub_sub");
+const QuestionView = require("./question.js");
 
+
+const GameView = function (container) {
+  this.container = container;
+}
+
+GameView.prototype.bindEvents = function () {
+  PubSub.subscribe("GameView:data-ready", (evt) => {
+    this.renderQuestion(evt.detail);
+  })
+};
+
+GameView.prototype.renderQuestion = function (question) {
+  this.container.innerHTML = "";
+  const newQuestion = new QuestionView(this.container);
+  newQuestion.render(question);
+}
+
+module.exports = GameView;
