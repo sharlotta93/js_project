6dc26cc8e5d26f1ee8eaad9ee30017e56e592131
Abigaila Ekierte <arekiert@gmail.com>
2019-03-09 10:40:54 +0000

    lower case keys in seeds file

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -4,22 +4,22 @@ db.dropDatabase();
 db.question.insertMany(
   [
     {
-      Genre: "History",
-      Question: "Which of these is a Greek temple?",
-      Answer: "Pantheon",
-      Hint: "Ancient Greek architechture features columns."
+      genre: "History",
+      question: "Which of these is a Greek temple?",
+      answer: "Pantheon",
+      hint: "Ancient Greek architechture features columns."
     },
     {
-      Genre: "Mathematics",
-      Question: "Paul has 7 boxes of 14 balls. He lost 6 balls. How many balls does he have now?",
-      Answer: 92,
-      Hint: "7 x 14 = 98. Now subtract 6!"
+      genre: "Mathematics",
+      question: "Paul has 7 boxes of 14 balls. He lost 6 balls. How many balls does he have now?",
+      answer: 92,
+      hint: "7 x 14 = 98. Now subtract 6!"
     },
     {
-      Genre: "Science",
-      Question: "What is the temperature at which water boils?",
-      Answer: 100,
-      Hint: "0 Celsius is the temperature at which water freezes. 5600 Celsius is the temperature of the Sun."
+      genre: "Science",
+      auestion: "What is the temperature at which water boils?",
+      answer: 100,
+      hint: "0 Celsius is the temperature at which water freezes. 5600 Celsius is the temperature of the Sun."
     }
   ]
 );
