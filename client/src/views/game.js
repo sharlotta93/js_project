f7bdcb95792ce62f0752a848e0ca223c05ad3351
Abigaila Ekierte <arekiert@gmail.com>
2019-03-09 12:10:17 +0000

    game view adding go back and forward buttons

--- client/src/views/game.js
+++ client/src/views/game.js
@@ -9,6 +9,7 @@ const GameView = function (container) {
 GameView.prototype.bindEvents = function () {
   PubSub.subscribe("GameView:data-ready", (evt) => {
     this.renderQuestion(evt.detail);
+    this.createFeatures();
   })
 };
 
@@ -16,19 +17,27 @@ GameView.prototype.renderQuestion = function (question) {
   this.container.innerHTML = "";
   const newQuestion = new QuestionView(this.container);
   newQuestion.render(question);
+
 }
 
-GameView.prototype.createFeatures = function (item) {
+GameView.prototype.createFeatures = function () {
   const gameContainer = document.createElement("div");
   gameContainer.id = 'game';
 
-  const buttonLeft = document.createElement("button", "GO BACK");
-  const buttonRight = document.createElement("button", "NEXT QUESTION");
+  const buttonLeft = this.createElement("button", "GO BACK");
+  buttonLeft.id = 'leftButton';
+  const buttonRight = this.createElement("button", "NEXT QUESTION");
+  buttonRight.id = 'rightButton';
   const deleteButton = this.createElement("button", "FINISH GAME");
+  deleteButton.id = 'deleteButton'
 
-  this.createButton(deleteButton, item);
+  this.createButton(deleteButton, item, "GamenView:finish-game");
+  this.createButton(buttonLeft, item, "GamenView:go-back-a-question");
+  this.createButton(buttonRight, item, "GamenView:next-question");
 
   gameContainer.appendChild(deleteButton);
+  gameContainer.appendChild(buttonLeft);
+  gameContainer.appendChild(buttonRight);
 
   this.container.appendChild(gameContainer);
 };
@@ -36,7 +45,7 @@ GameView.prototype.createFeatures = function (item) {
 GameView.prototype.createButton = function (button, item, channel) {
   button.value = item._id;
   button.addEventListener('click', (evt) => {
-    PubSub.publish("GamenView:finish-game", evt.target.value)
+    PubSub.publish(channel, evt.target.value)
   })
   return button
 };
