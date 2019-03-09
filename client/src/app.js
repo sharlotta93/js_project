e87bf885c1ba53eb1b2d4e8ff0bb6f5845195038
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-03-09 13:02:39 +0000

    fixing initialLoad in model

--- client/src/app.js
+++ client/src/app.js
@@ -3,6 +3,6 @@ const GameLogic = require('./models/game_logic.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   const gameLogic = new GameLogic();
+  console.log('Loaded');
   gameLogic.prepareQuestions();
-  gameLogic.initialLoad()
 })
--- client/src/models/game_logic.js
+++ client/src/models/game_logic.js
@@ -3,34 +3,31 @@ const PubSub = require('../helpers/pub_sub.js');
 
 const GameLogic = function () {
   this.questions = [];
-  this.request = new RequestHelper('/api/game');
+  this.request = new RequestHelper('http://localhost:3000/api/game');
 }
 
 GameLogic.prototype.prepareQuestions = function () {
-  this.request
-    .get()
+  this.request.get()
     //get all questions from database
     .then((questions) => {
-      console.log(questions);
-      //assign data received to the array
       this.questions = questions;
+      this.initialLoad(questions);
+      //assign data received to the array
     })
-
     .catch((err) => console.error(err));
 };
-GameLogic.prototype.initialLoad = () => {
+GameLogic.prototype.initialLoad = (questions) => {
   const startButton = document.querySelector('#start-game');
-  startButton.addEventListener('click', onStartButtonClick)
+  console.log(questions);
+  startButton.addEventListener('click', this.onStartButtonClick);
+
 };
 
 GameLogic.prototype.onStartButtonClick = () => {
     startButton.classList.add('hidden');
     PubSub.publish('Game:initial-question', this.questions[0])
-    console.log(this.questions[0]);
-};
-
-
-
-GameLogic.prototype.onStartButtonClick = function () {
 
 };
+
+
+module.exports = GameLogic;
