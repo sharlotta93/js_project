e79de496f94000001537279da8db6f03080f8ba6
Abigaila Ekierte <arekiert@gmail.com>
2019-03-10 17:41:55 +0000

    answer type added to seeds file

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -4,18 +4,21 @@ db.dropDatabase();
 db.question.insertMany(
   [
     {
+      answer_type: "picture",
       genre: "History",
       question: "Which of these is a Greek temple?",
       answer: "Pantheon",
       hint: "Ancient Greek architechture features columns."
     },
     {
+      answer_type: "number",
       genre: "Mathematics",
       question: "Paul has 7 boxes of 14 balls. He lost 6 balls. How many balls does he have now?",
       answer: 92,
       hint: "7 x 14 = 98. Now subtract 6!"
     },
     {
+      answer_type: "number",
       genre: "Science",
       question: "What is the temperature at which water boils?",
       answer: 100,
@@ -23,3 +26,5 @@ db.question.insertMany(
     }
   ]
 );
+
+// other answer_type is "text"
