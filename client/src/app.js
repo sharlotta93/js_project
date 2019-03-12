44104700c5daff114c6bc9f04f0f46d3b7c8a5d5
Abigaila Ekierte <arekiert@gmail.com>
2019-03-12 14:54:26 +0000

    bug fixed

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,16 @@
 const GameLogic = require('./models/game_logic.js');
 const GameView = require("./views/game.js");
 const Countries = require("./models/countries.js")
+const PubSub = require('./helpers/pub_sub.js');
 
 
-document.addEventListener('DOMContentLoaded', () => {  
+document.addEventListener('DOMContentLoaded', () => {
+  const startButton = document.querySelector('#start-game');
+  startButton.addEventListener('click', () => {
+    startButton.classList.add('hidden');
+    PubSub.publish('Game:data-ready');
+  });
+
   const gameContainer = document.querySelector("#game-container");
   const gameView = new GameView(gameContainer);
   gameView.bindEvents();
