89b762e339057882e8b4a824c7251acbc5fb988b
Abigaila Ekierte <arekiert@gmail.com>
2019-03-12 11:08:48 +0000

    successfull fetch from API

--- client/src/app.js
+++ client/src/app.js
@@ -1,11 +1,16 @@
 const GameLogic = require('./models/game_logic.js');
 const GameView = require("./views/game.js");
+const Countries = require("./models/countries.js")
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const gameContainer = document.querySelector("#game-container");
   const gameView = new GameView(gameContainer);
   gameView.bindEvents();
+
+  const countries = new Countries();
+  countries.getData();
+  console.log(countries);
 
   const gameLogic = new GameLogic();
   console.log('Loaded');
