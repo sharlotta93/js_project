a65c7bf195d0ad1fd597311583d53e37faea33cd
Abigaila Ekierte <arekiert@gmail.com>
2019-03-12 16:06:29 +0000

    swapped maps around

--- client/src/models/countries.js
+++ client/src/models/countries.js
@@ -47,9 +47,9 @@ Countries.prototype.createQuestionObject = function (countries) {
       answerType: "picture",
       genre: "Geography",
       question: `Which of these is the flag of ${questionObject1.name}?`,
-      image1: questionObject1.flag,
-      image2: questionObject2.flag,
-      answer: "image-left",
+      image1: questionObject2.flag,
+      image2: questionObject1.flag,
+      answer: "image-right",
       hint: `The language of that country is ${questionObject1.language} and the capital is ${questionObject1.capital}.`
   }
   return newQuestion;
